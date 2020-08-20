9cbaf4ea5895a107657db7bd8184eeb80fd23856
sriraj33 <69248474+sriraj33@users.noreply.github.com>
2020-08-20 17:44:15 +0530

    Update character_freq.py

--- character_freq.py
+++ character_freq.py
@@ -3,8 +3,7 @@ def most_frequent(s):
     dic = {}
     for n in s:
         z = s.count(n)
-        dic[n] = str(z)
-                                                 
+        dic[n] = str(z)                                           
     tuple1 = tuple(dic)
     tuple1 =sorted(dic.items(), reverse = True, key = lambda x : x[1])
     print("Output:\n")
