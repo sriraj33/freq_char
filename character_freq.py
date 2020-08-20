e6e0a8b84b668ca2071185a8401319a8dfa36d76
sriraj33 <69248474+sriraj33@users.noreply.github.com>
2020-08-20 17:42:51 +0530

    Add files via upload

---
+++ character_freq.py
@@ -0,0 +1,16 @@
+def most_frequent(s):
+    
+    dic = {}
+    for n in s:
+        z = s.count(n)
+        dic[n] = str(z)
+                                                 
+    tuple1 = tuple(dic)
+    tuple1 =sorted(dic.items(), reverse = True, key = lambda x : x[1])
+    print("Output:\n")
+    for x in tuple1:
+        print(x[0] + "=" +x[1])
+
+          
+s = input("enter a string: ")
+most_frequent(s)
